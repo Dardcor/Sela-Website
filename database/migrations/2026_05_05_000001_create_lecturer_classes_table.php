c135052aa6adede86d0540287bc64f9841d75946
acalypha9 <fahroldhisukirno9@gmail.com>
2026-05-06 17:35:49 +0700

    fix schema

--- database/migrations/2026_05_05_000001_create_lecturer_classes_table.php
+++ database/migrations/2026_05_05_000001_create_lecturer_classes_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('lecturer_classes')) {
+            return;
+        }
+
         Schema::create('lecturer_classes', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('lecturer_id');
